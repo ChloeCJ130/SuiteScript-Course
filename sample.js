57c40c73962355cc6d2fdf835f7bb4cd72592cb7
ChloeCJ130 <chloecamacho130@gmail.com>
2026-01-26 14:52:07 +0800

    Task 2 and 3
    From Case Study

--- sample.js
+++ sample.js
@@ -1,1 +1,10 @@
-//a sample file
+//Activty Tast 2
+
+var arr1 = "Netsuite".split('');
+var arr2 = arr1.reverse();
+var arr3 = "Cloudtech".split('');
+
+arr2.push(arr3);
+
+console.log("array 1: length = " + arr1.length + " last = " + arr1.slice(-1));
+console.log("array 2: length = " + arr2.length + " last = " + arr2.slice(-1));
